f7a9e09c7848c31d1f28d322da40d62fd9353570
Vansh111000 <vanshskataria@gmail.com>
2025-04-09 00:13:12 +0530

    fix: update welcome message for clarity

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,8 @@ export default function Index() {
     <View
      className={"flex-1 items-center justify-center"}>
 
-      <Text className={"text-5xl text-blue-800 font-bold"}>Welcome to our movie app. It Will practice more fr</Text>
+      <Text className={"text-5xl text-blue-800 font-bold"}>Welcome to our movie app.</Text>
+      <Text className={"text-4xl text-blue-800 "}>1st Movie.</Text>
     </View>
   );
 }
