cfb71b9b0de0cb1f15557cbf922c861db8519a31
Vansh111000 <vanshskataria@gmail.com>
2025-04-09 23:56:24 +0530

    feat: add onboarding screen and link from index component

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -6,6 +7,9 @@ export default function Index() {
             className={"flex-1 items-center justify-center"}>
 
             <Text className={"text-5xl text-dark-200 font-bold"}>Welcome</Text>
+            <Link href={"/onboarding"}>
+                <Text className={"text-2xl text-dark-100 font-bold"}>Get Started</Text> 
+            </Link>
 
         </View>
     );
