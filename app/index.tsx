14057fda0c1c82baa103792afa0d7191b97e56de
Vansh111000 <vanshskataria@gmail.com>
2025-04-09 22:55:31 +0530

    fix: remove redundant movie title text from index component

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ export default function Index() {
      className={"flex-1 items-center justify-center"}>
 
       <Text className={"text-5xl text-blue-800 font-bold"}>Welcome to our movie app.</Text>
-      <Text className={"text-4xl text-blue-800 font-semibold  "}>1st Movie.</Text>
+      
     </View>
   );
 }
