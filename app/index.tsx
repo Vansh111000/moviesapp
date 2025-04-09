ab5f650ea0d34ca4baeb0b0dd73931b2c548ae84
Vansh111000 <vanshskataria@gmail.com>
2025-04-09 23:36:19 +0530

    fix: update welcome message for clarity and remove redundant text; add color definitions to Tailwind CSS configuration

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,12 @@
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-     className={"flex-1 items-center justify-center"}>
+    return (
+        <View
+            className={"flex-1 items-center justify-center"}>
 
-      <Text className={"text-5xl text-blue-800 font-bold"}>Welcome to 1st our movie app. app app</Text>
-      
-    </View>
-  );
+            <Text className={"text-5xl text-dark-200 font-bold"}>Welcome</Text>
+
+        </View>
+    );
 }
