4bc7a6c52406c99784df0b1f19ca9a789ed30d75
Vansh111000 <vanshskataria@gmail.com>
2025-04-10 17:49:47 +0530

    feat: enhance app layout with transparent status bar and updated navigation bar styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,24 @@
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { setBackgroundColorAsync } from 'expo-system-ui';
+import { useEffect } from 'react';
+import { Image, ScrollView, StatusBar, View } from "react-native";
 
 export default function Index() {
+    useEffect(() => {
+        // Set navigation bar color to transparent
+        setBackgroundColorAsync('transparent');
+    }, []);
+
     return (
-        <View
-            className={"flex-1 items-center justify-center"}>
+        <View className="flex-1 bg-primary">
+            <Image source={images.bg} className="absolute w-full z-0" />
+            <StatusBar translucent backgroundColor="transparent" />
 
-            <Text className={"text-5xl text-dark-200 font-bold"}>Welcome!</Text>
+            <ScrollView className="flex-1  px-5 ">
+                <Image  source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
 
-
+            </ScrollView>
         </View>
     );
 }
