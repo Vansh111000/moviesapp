727d0f861bbd3369b4e159e3bd968dc306758c67
Vansh111000 <vanshskataria@gmail.com>
2025-04-12 00:25:53 +0530

    fix: update android navigation bar styles for better visibility

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,17 @@
 import { Stack } from "expo-router";
+import * as SystemUI from 'expo-system-ui';
+import { useEffect } from 'react';
 import './globals.css';
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+  useEffect(() => {
+    SystemUI.setBackgroundColorAsync('#151312');
+  }, []);
 
-  </Stack>;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
